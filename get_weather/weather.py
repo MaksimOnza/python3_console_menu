980fe976f73127fc9ab161db3cab1c4ca97eb3ef
MaksimOnza <maximus83onza@gmail.com>
2020-01-16 00:57:51 +0200

    Remembe all that was lost

--- get_weather/weather.py
+++ get_weather/weather.py
@@ -1,8 +1,10 @@
 import http.client
-import requests
+import json
+#from pprintpp import pprint
+#import requests
 
 key = '719d3fe6203f73509524e0bca348cb53'
-city = "London"
+city = "Rzhavchik"
 mode = 'current'
 hist_date = '2015-21-01'
 
@@ -10,5 +12,6 @@ conn = http.client.HTTPConnection("api.weatherstack.com")
 conn.request("GET", f'/{mode}?access_key={key}&query={city}&historical_date={hist_date}')
 output = conn.getresponse()
 body = output.read()
-temperature = "temperature"
-print(body)
+loaded_json = json.loads(body)
+
+print(loaded_json['current']['temperature'])
