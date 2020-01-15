f0963b73caa873560ce9c8c5b19363c5cf9ca8a6
MaksimOnza <maximus83onza@gmail.com>
2020-01-15 23:11:20 +0200

    deleted example

--- get_weather/html_weather.py
+++ get_weather/html_weather.py
@@ -6,13 +6,3 @@ page = requests.get(f'https://sinoptik.ua/погода-харьков')
 tree = html.fromstring(page.content)
 some = tree.xpath('//p[@class="today-temp"]/text()')
 print(some[0])
-
-page = requests.get('http://econpy.pythonanywhere.com/ex/001.html')
-tree = html.fromstring(page.content)
-
-#This will create a list of buyers:
-buyers = tree.xpath('//div[@title="buyer-name"]/text()')
-#This will create a list of prices
-prices = tree.xpath('//span[@class="item-price"]/text()')
-print(buyers)
-print(prices)
