78e1315a782a48736a17140e24e6c3460e6f9634
MaksimOnza <maximus83onza@gmail.com>
2020-01-25 21:15:16 +0200

    addedindex file

---
+++ get_weather/data/key_symbol.py
@@ -0,0 +1,7 @@
+class KeySymbol:
+
+	KEY_SELECT_CITY = 'c'
+	KEY_SELECT_WEB = 'w'
+	KEY_SHOW_WEATHER = 'a'
+	KEY_EXIT = 'q'
+	DICT_KEY_WEB = {'weatherstack.com': '1','openweathermap.org': '2', 'worldweatheronline.com': '3'}
