fc777f1da79b19d95f5c98e963b5ac4f40c3eb96
lenovo <maximus83onza@gmail.com>
2020-02-13 13:03:28 +0200

    rewrite parser with urllib.parse

--- get_weather/server_http.py
+++ get_weather/server_http.py
@@ -5,15 +5,16 @@ from resources.weatherstack import WeatherstackResource
 from resources.worldweatheronline import WorldweatheronlineResource
 from resources.sinoptik import SinoptikResource
 import config
+import urllib.parse as urllib
 
 PORT = 8000
 CITY = 'city'
 RESOURCE = 'resource'
 resources = {
-    'weatherstack': WeatherstackResource(config.WEATHERSTACK_ACCESS_KEY),
-    'openweathermap': OpenweathermapResource(config.OPENWEATHERMAP_ACCESS_KEY),
-    'worldweatheronline': WorldweatheronlineResource(config.WORLDWEATHERONLINE_ACCESS_KEY),
-    'sinoptik': SinoptikResource()
+	'weatherstack': WeatherstackResource(config.WEATHERSTACK_ACCESS_KEY),
+	'openweathermap': OpenweathermapResource(config.OPENWEATHERMAP_ACCESS_KEY),
+	'worldweatheronline': WorldweatheronlineResource(config.WORLDWEATHERONLINE_ACCESS_KEY),
+	'sinoptik': SinoptikResource()
 }
 
 Handler = http.server.BaseHTTPRequestHandler
@@ -27,27 +28,36 @@ class HttpProcessor(http.server.BaseHTTPRequestHandler):
 		self.send_response(200)
 		self.send_header('content-type','text/html')
 		self.end_headers()
-		self.parsing(self.path)
+		
+		self.url_parsing(self.path)
+
 		self.temperature = self.get_temperature(self.resource, self.city)
 		self.wfile.write('Temperature in '.encode())
-		self.wfile.write(self.city.encode())
+		self.wfile.write((self.listToString(self.city)).capitalize().encode())
 		self.wfile.write(' => '.encode())
 		self.wfile.write(str(self.temperature).encode())
+		self.wfile.write('*C'.encode())
 		
-
-	def parsing(self, string):
-		first_symbol = string.find("?")
-		start_param = string.find(RESOURCE, first_symbol)
-		end_param = string.find("&", start_param)
-		self.resource = string[start_param+len(RESOURCE)+1: end_param]
-		start_param = string.find(CITY, end_param)
-		self.city = string[start_param+len(CITY)+1: len(string)]
-
+	
 	def get_temperature(self, resource, city):
-		temperature = resources[resource].get_temperature(city)
+		temperature = resources[self.listToString(resource)].get_temperature(self.listToString(city))
 		return temperature
+
+
+	def url_parsing(self, url):
+		base_url = urllib.urlparse(url)
+		split_after_url = base_url.query.split(' ')[0]
+		dict_param = urllib.parse_qs(split_after_url)
+		self.resource = dict_param[RESOURCE]
+		self.city = dict_param[CITY]
+		return dict_param
+
+
+	def listToString(self, s):
+		str1 = ""  
+		return (str1.join(s)) 
+	
+
 
 serv = http.server.HTTPServer(("localhost",PORT),HttpProcessor)
 serv.serve_forever()
-
-
